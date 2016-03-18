a3253507871f3d865da5d895853039c08ed29fe0
Lucía Velasco <Luca Velasco>
2016-03-18 03:37:17 -0600

    First commit

--- ASDF.py
+++ ASDF.py
@@ -1,32 +1,34 @@
 import time
+import sensors
+import pyupm_i2clcd as lcd
 import pyupm_ttp223 as ttp223
 import pyupm_servo as servo
 import pyupm_grove as grove
+import paho.mqtt.client as mqtt
 
 
 gServo = servo.ES08A(5)
 button = grove.GroveButton(6)
 touch = ttp223.TTP223(7)
+display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+mqttc = mqtt.Client("zone_2")
+mqttc.connect("10.43.28.194")
+regSpaces = 17
+available = 17
 
-def checkTouchPulse(touch):
-    if touch.isPressed():
-        while True:
-            if not touch.isPressed():
-                return True
-    return False
 
-def doubleTouchPulse(touch):
-    while True:
-        if checkTouchPulse(touch):
-            while True:
-                if checkTouchPulse(touch):
-                    return True
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("zone_1")
 
-while(True):
-    if button.value() == 1:
-        gServo.setAngle(90)
-        time.sleep(1)
-        gServo.setAngle(0)
+def on_message(client, userdata, message):
+    available -= 1
+    displayUpdate(display, avSpaces, regSpaces)
 
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+while(True)
     if doubleTouchPulse(touch):
-        print "Yey"
+        avSpaces +=1
+        mqttc.publish("zone_1", "zone_2")
+        displayUpdate(display, avSpaces, regSpaces)
---
+++ sensors.py
@@ -0,0 +1,44 @@
+import pyupm_ttp223 as ttp223
+import pyupm_grove as grove
+
+def checkTouchPulse(touch):
+    if touch.isPressed():
+        while True:
+            if not touch.isPressed():
+                return True
+    return False
+
+def checkButtonPulse(button):
+if button.value() != 0:
+        while True:
+            if button.value() == 0:
+                return True
+    return False
+
+def doubleTouchPulse(touch):
+    while True:
+        if checkTouchPulse(touch):
+            while True:
+                if checkTouchPulse(touch):
+                    return True
+
+def displayUpdate(display, avSpaces, regSpaces):
+        display.clear()
+        if avSpaces > 0:
+            if avSpaces >= regSpaces / 2:
+                display.setColor(0, 255, 0)
+            elif avSpaces >= regSpaces / 4:
+                display.setColor(255, 255, 0)
+            else:
+                display.setColor(255, 127, 0)
+
+            display.setCursor(0, 0)
+            display.write("Available places:")
+            display.setCursor(1, 0)
+            display.write(str(avSpaces))
+        else:
+            display.setColor(255, 0, 0)
+            display.setCursor(0, 0)
+            display.write("No available")
+            display.setCursor(1, 0)
+            display.write("places")
