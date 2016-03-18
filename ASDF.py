5d649a588cd2d07ac6665436c415ce3dbed0cbcc
Lucía Velasco <Luca Velasco>
2016-03-18 08:58:52 -0600

    D:

--- ASDF.py
+++ ASDF.py
@@ -4,7 +4,6 @@ import pyupm_i2clcd as lcd
 import pyupm_ttp223 as ttp223
 import paho.mqtt.client as mqtt
 
-
 touch = ttp223.TTP223(7)
 display = lcd.Jhd1313m1(0, 0x3E, 0x62)
 mqttc = mqtt.Client("zone_2")
@@ -13,7 +12,7 @@ regSpaces = 17
 avSpaces = regSpaces
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("zone_1")
+    client.subscribe("zone_2")
 
 def on_message(client, userdata, message):
     available -= 1
@@ -23,6 +22,7 @@ mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
 while(True):
+    print sensors.doubleTouchPulse(touch) 
     if sensors.doubleTouchPulse(touch):
         avSpaces +=1
         mqttc.publish("zone_1", "zone_2")
